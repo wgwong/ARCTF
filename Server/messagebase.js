80c40dbf7631541076f4725b67df3ae8b2c1f2eb
unknown <wgwong@mit.edu>
2016-10-09 17:25:20 -0400

    added playerUpdate function to messagebase

--- Server/messagebase.js
+++ Server/messagebase.js
@@ -1,20 +1,56 @@
 //var User = require('./models/schema').User;
 
-var loggedInUsers = {};
+//var loggedInUsers = {};
 
-var messages = {}; //key = target_user, value = dictionary where key = src, value = array of message tuples (msg, date, self?) including self, msg
+//var messages = {}; //key = target_user, value = dictionary where key = src, value = array of message tuples (msg, date, self?) including self, msg
+
+
+userData = {}; //key = user
+//vaue = dictionary
 
 var Messagebase = (function Messagebase() {
 	var that = Object.create(Messagebase.prototype);
 
 	var io = null;
 
+	var COORD_SIZE_CAP = 20;
+
 	that.setIO = function(IO) {
 		io = IO;
 
+		console.log("msg base set io");
+
+		console.log("is it null? ", io == null);
+		console.log("is it undef? ", io == undefined);
+
 		//io connection
 		io.on('connection', function(socket){
+			console.log("connection established");
+
 			socket.on('disconnect', function(){
+			});
+
+			socket.on('session', function(msg){
+				console.log("message received: ", msg);
+				io.emit("session", "connect world");
+			});
+
+			socket.on('playerUpdate', function(data) {
+				player = data.address;
+				latitude = data.latitude;
+				longitude = data.longitude;
+
+				if (!(player in userData)) {
+					userData[player] = {'name': 'test', 'team': 'gray', 'coordinates': []};
+				}
+
+				if (userData[player]['coordinates'].length > COORD_SIZE_CAP) {
+					userData[player]['coordinates'].pop(0);
+					userData[player]['coordinates'].push([latitude, longitude]);
+				}
+
+				//DEBUG
+				io.emit("playerUpdate_confirm", [true, userData]);
 			});
 
 			socket.on('chat message', function(msg, from, to, date) {
@@ -36,6 +72,10 @@ var Messagebase = (function Messagebase() {
 				io.emit("chat message", msg, from, to, date);
 			});
 		});
+
+		setInterval( () => {
+			io.emit("session", "this is a timed message " + (new Date().getTime()));
+		}, 1000);
 	}
 
 	that.login = function(username) {
