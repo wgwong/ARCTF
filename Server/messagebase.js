18bc7582fb0540b422333b7f7fea4a46258a6c89
unknown <wgwong@mit.edu>
2016-10-25 02:22:06 -0400

    trimmed messagebase.js

--- Server/messagebase.js
+++ Server/messagebase.js
@@ -1,10 +1,3 @@
-//var User = require('./models/schema').User;
-
-//var loggedInUsers = {};
-
-//var messages = {}; //key = target_user, value = dictionary where key = src, value = array of message tuples (msg, date, self?) including self, msg
-
-
 userData = {};
 /*
 	key = userKey
@@ -173,64 +166,6 @@ var Messagebase = (function Messagebase() {
 			}
 		}, 20000);
 
-	}
-
-	that.login = function(username) {
-		loggedInUsers[username] = true;
-		if (io) {
-			io.emit("login user", username);
-		}
-	}
-
-	that.logout =  function(username) {
-		loggedInUsers[username] = false;
-		if (io) {
-			io.emit("logout user", username);
-		}
-	}
-
-	that.getActiveUsers = function() {
-		return loggedInUsers;
-	}
-
-	that.getOfflineUsers = function(cb) {
-		User.getAllUsers(function(err, result) {
-			if (err) {
-				console.error(err);
-				cb(err);
-			} else {
-				if (result.length > 0) {
-					var userList;
-					if (result.length > 1) {
-						userList = result.reduce(function(prev, cur, i, arr) {
-							if (i == 1) {
-								return [prev.username, cur.username]
-							} else {
-								return prev.concat(cur.username);
-							}
-						});
-					} else {
-						userList = [result[0].username];
-					}
-					var offlineUsers = userList.filter(function(user) {
-						return Object.keys(loggedInUsers).indexOf(user) === -1 || !loggedInUsers[user];
-					});
-					cb(null, offlineUsers);
-				} else {
-					cb(null, []);
-				}
-			}
-		});
-	}
-
-	//get all messages associated with the specified user
-	that.getMessagesByUsername = function(username) {
-
-		if (Object.keys(messages).indexOf(username) !== -1) {
-			return messages[username];
-		} else {
-			return {};
-		}
 	}
 
 	Object.freeze(that); //prevent any further modifications to the member fields and methods of this class
