c439158a41d9e1dc6a10d223ca4f775d8f502c2b
nicko96 <nicko96@mit.edu>
2016-11-21 15:56:14 -0500

    Counter timer added and icons changed

--- ARCTFdemo/app/src/main/java/com/demo/arctf/arctfdemo/MainActivity.java
+++ ARCTFdemo/app/src/main/java/com/demo/arctf/arctfdemo/MainActivity.java
@@ -6,11 +6,19 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView img= (ImageView) findViewById(R.id.Towers_Controlled);
+        img.setImageResource(R.drawable.blue_capture_point-web);
+        ImageView img= (ImageView) findViewById(R.id.Opponent_Towers_Controlled);
+        img.setImageResource(R.drawable.orange_capture_point-web);
+        chronometer = (Chronometer) findViewById(R.id.Game_Time);
+        chronometer.setBase(300000);
+        chronometer.start();
     }
 
 }
